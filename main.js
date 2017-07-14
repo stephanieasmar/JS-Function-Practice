ae1a6f943f5d03b3160095337c34298ad305532a
Stephanie Asmar <sasmar8@gmail.com>
2017-07-14 16:06:20 -0400

    Added more exercises

--- main.js
+++ main.js
@@ -143,8 +143,6 @@ oddNum = nums.filter((n) => {
 // ];
 
 
-// console.log(cities[0]["temperature"]);
-
 // var newArray = [];
 
 // newArray = cities.filter((n) => {
@@ -154,28 +152,43 @@ oddNum = nums.filter((n) => {
 // console.log(newArray);
 
 
-
-// Cities 2
+// #3a Cities 2: NOT COMPLETED
 
 // Write a function which takes an array of city objects like the 
 // above problem as input and returns an array of the cities names.
 
 
-var cities = [
-  { name: 'Los Angeles', temperature: 60.0},
-  { name: 'Atlanta', temperature: 52.0 },
-  { name: 'Detroit', temperature: 48.0 },
-  { name: 'New York', temperature: 80.0 }
+// #4 Good Job:
+// Given an array of people's names:
+// Print out 'Good Job, {{name}}!' for each person's name, one on a line.
+
+var people = [
+  'Dom',
+  'Lyn',
+  'Kirk',
+  'Autumn',
+  'Trista',
+  'Jesslyn',
+  'Kevin',
+  'John',
+  'Eli',
+  'Juan',
+  'Robert',
+  'Keyur',
+  'Jason',
+  'Che',
+  'Ben'
 ];
 
+var goodJob = [];
 
-// console.log(cities[0]["temperature"]);
 
-var newArray = [];
-
-newArray = cities.filter((n) => {
-    return n.temperature < 70.0;
+goodJob = people.map((n) => {
+    return ("Good job: " + n);
 });
 
-// console.log(newArray);
+// console.log(goodJob.join("\n"));
 
+goodJob.forEach((n) => {
+    console.log(n);
+})
